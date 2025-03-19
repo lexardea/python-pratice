6fe9656cabd74df314bb0352748f511f92946904
Alexis Thompson <alexismariet85@gmail.com>
2025-03-19 15:40:22 -0400

    Update codewars-solutions.py

--- codewars-solutions.py
+++ codewars-solutions.py
@@ -1,12 +1,14 @@
 #return even or odd
-number = input("type a number and then press enter: ")
-
 def even_or_odd(number):
-
-    if number % 2 == 0: print("Even")
-    else: print("Odd")
+    if number % 2 == 0: return "even"
+    else: return "odd"
 
 #return integer as a string
 def number_to_string(num):
     # Return a string of the number here!
     return str(num)
+
+#remove all whitespace from string
+def no_space(x):
+    newx = x.replace(" ", "")
+    return newx
